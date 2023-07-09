fab138eb00a3d4ee05d198a8aada1ad8a26ab011
Jay-Khakim <mgmediajay@gmail.com>
2023-07-10 01:15:45 +0900

    chat is ready

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,14 @@
-const ChatsPage = ()=>{
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced' 
+
+const ChatsPage = (props )=>{
+    const chatProps = useMultiChatLogic('40248dc4-4649-4f9e-8f42-eb5ae7eb1d0b', props.user.username, props.user.secret);
+    return (
+    <div style={{height: '100vh'}}> 
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps}  style={{height: '100%'}}/>
+    </div>
+    )
+    
 }
 
 export default ChatsPage
