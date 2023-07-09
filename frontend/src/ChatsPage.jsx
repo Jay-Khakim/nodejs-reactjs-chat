d24bf8783053d699e7a82d9b7216f21714974b54
Jay-Khakim <mgmediajay@gmail.com>
2023-07-10 01:24:23 +0900

    pretty UI

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,14 +1,15 @@
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced' 
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
-const ChatsPage = (props )=>{
-    const chatProps = useMultiChatLogic('40248dc4-4649-4f9e-8f42-eb5ae7eb1d0b', props.user.username, props.user.secret);
-    return (
-    <div style={{height: '100vh'}}> 
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow {...chatProps}  style={{height: '100%'}}/>
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        username={props.user.username}
+        secret={props.user.secret}
+      />
     </div>
-    )
-    
-}
+  );
+};
 
-export default ChatsPage
+export default ChatsPage;
